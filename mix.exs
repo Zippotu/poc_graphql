4e2c71e6e5d38f52b6625de416f45f49fa5910f6
Alexandre Pereira <lexandre32@gmail.com>
2022-03-16 09:13:04 -0300

    db setup

--- mix.exs
+++ mix.exs
@@ -33,6 +33,8 @@ defmodule PocGraphql.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"},
       {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
---
+++ test/support/fixtures/news_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule PocGraphql.NewsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `PocGraphql.News` context.
+  """
+
+  @doc """
+  Generate a link.
+  """
+  def link_fixture(attrs \\ %{}) do
+    {:ok, link} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        url: "some url"
+      })
+      |> PocGraphql.News.create_link()
+
+    link
+  end
+end
